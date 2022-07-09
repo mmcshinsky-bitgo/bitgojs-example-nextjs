a5b34654bbb6b6904995abcedeb442fe69eb5a47
Michael <michaelmcshinsky@bitgo.com>
2022-07-08 23:05:58 -0600

    feat: init

---
+++ next.config.js
@@ -0,0 +1,51 @@
+/** @type {import('next').NextConfig} */
+
+const path = require("path");
+const webpack = require("webpack");
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        // this is only required if using bitgo instead of just the sdk-api
+        "@hashgraph/sdk": path.resolve(
+          "./node_modules/@hashgraph/sdk/src/browser.js"
+        ),
+        "superagent-proxy": false,
+      },
+      fallback: {
+        ...config.resolve.fallback,
+        constants: false,
+        crypto: require.resolve("crypto-browserify"),
+        dns: false,
+        fs: false,
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        http2: require.resolve("stream-http"),
+        net: false,
+        os: false,
+        path: false,
+        stream: require.resolve("stream-browserify"),
+        tls: false,
+        url: require.resolve("url/"),
+        vm: false,
+        zlib: false,
+      },
+    };
+    config.externals = [...config.externals, "superagent-proxy"];
+    config.plugins = [
+      ...config.plugins,
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"],
+        process: "process/browser",
+      }),
+    ];
+    return config;
+  },
+};
+
+module.exports = nextConfig;
